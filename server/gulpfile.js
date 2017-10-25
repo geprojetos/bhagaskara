530ad6761f43a6606ae75fb4d92092718bc5cfa5
Genilson <geprojetosbrasil@gmail.com>
2017-10-25 18:01:31 -0200

    Iniciando Projeto Bhagaskara

---
+++ server/gulpfile.js
@@ -0,0 +1,155 @@
+var gulp            = require("gulp"),
+    clean           = require("gulp-clean"),
+    browserSync     = require("browser-sync"),
+    jade            = require("gulp-jade"),
+    plumber         = require("gulp-plumber"),
+    sass            = require("gulp-sass"),
+    autoprefixer    = require('gulp-autoprefixer')
+    cssLint         = require("gulp-csslint"),
+    useref          = require("gulp-useref"),
+    mmq             = require('gulp-merge-media-queries'),
+    inlinesource    = require('gulp-inline-source'),
+    sprite          = require('gulp.spritesmith'),
+    jshint          = require("gulp-jshint"),
+    jsStylish       = require("jshint-stylish"),
+    uglify          = require("gulp-uglify");
+
+gulp.task("clean", function(){
+
+    gulp.src(["../app/_public", "../app/_source/compilado"])
+    .pipe(clean({force: true}));
+});
+
+
+gulp.task("server", function(){
+
+    browserSync.init({
+        server: {
+            baseDir: "../app/_public"
+        }
+    });
+
+    gulp.watch("../app/_public/**/*").on("change", browserSync.reload);
+    gulp.watch("../app/_source/**/*.jade", ["jade"]);
+    gulp.watch("../app/_source/**/*.scss", ["sass"]);
+    gulp.watch("../app/_source/js/*.js", ["js"]);
+    gulp.watch("../app/_source/img/*", ["img"]);
+    gulp.watch("../app/_source/img/sprite/*.png", ["sprite"]);
+
+    gulp.watch("../app/_source/compilado/*.html", ["build-html"]);
+    gulp.watch("../app/_source/compilado/css/*.css", ["build-css", "build-html"]);
+    gulp.watch("../app/_source/compilado/js/*.js", ["build-js", "build-html"]);
+    gulp.watch("../app/_source/vendor/*.js", ["build-vendor", "build-html"]);
+    
+    gulp.watch("../server/gulpfile.js", ["build-html", "build-css", "build-js"]);
+    
+});
+
+
+gulp.task("jade", function(){
+
+    gulp.src("../app/_source/index.jade")
+        .pipe(plumber())
+        .pipe(jade({
+             pretty: true // 'true' não minifica o html, 'false' minifica html
+        }))
+        .pipe(gulp.dest("../app/_source/compilado"));
+});
+
+
+gulp.task("sass", function(){
+
+    gulp.src(["../app/_source/scss/layout/abouve-default.scss", "../app/_source/scss/layout/style.scss"])
+        .pipe(sass({outputStyle: 'compact' /* expanded, compact,compressed*/}).on('error', sass.logError))
+        .pipe(plumber())
+         .pipe(autoprefixer({
+                browsers: ['last 10 versions'],
+                cascade: false
+            }))
+        .pipe(mmq({
+            beautify: false
+        }))
+        .pipe(sass({outputStyle: 'compact' /* expanded, compact,compressed*/}).on('error', sass.logError))
+        .pipe(gulp.dest('../app/_source/compilado/css'))
+});
+
+
+gulp.task("js", function(){
+
+    gulp.src("../app/_source/js/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter(jsStylish))
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("../app/_source/compilado/js"))
+});
+
+
+gulp.task("img", function(){
+
+    gulp.src("../app/_source/img/*")
+        .pipe(gulp.dest("../app/_public/img"))
+});
+
+
+gulp.task('sprite', function () {
+
+    var spriteData = gulp.src('../app/_source/img/sprite/*.png')
+        .pipe(sprite({
+            imgName: 'sprite.png',  //nome sprite gerado
+            cssName: 'sprite.css'   // nome css do sprite
+        }));
+        spriteData.img.pipe(gulp.dest('../app/_public/img/sprite'));
+        spriteData.css.pipe(gulp.dest('../app/_source/cssSprite'));
+});
+
+
+gulp.task("build-html", function(){
+
+    gulp.src("../app/_source/compilado/*.html")
+        .pipe(useref())
+        .pipe(inlinesource())
+        .pipe(gulp.dest("../app/_public"))
+});
+
+
+gulp.task("build-css", function(){
+
+    gulp.src("../app/_source/compilado/css/*.css")
+        // .pipe(cssLint())
+        // .pipe(cssLint.formatter())
+});
+
+
+gulp.task("build-js", function(){
+
+    gulp.src("../app/_source/compilado/js/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter(jsStylish))
+        .pipe(plumber())
+        .pipe(uglify())
+});
+
+
+gulp.task("build-fonts", function(){
+
+    gulp.src("../app/_source/fonts/**/*")
+    .pipe(gulp.dest("../app/_public/fonts"));
+});
+
+
+gulp.task("build-vendor", function(){
+
+    gulp.src("../app/_source/vendor/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("../app/_public/vendor"));
+});
+
+
+gulp.task("comp", ["jade", "sass", "js"]);
+
+gulp.task("images", ["img", "sprite"]);
+
+gulp.task("build", ["build-html", "build-css", "build-js", "build-fonts", "build-vendor"]);
+
+gulp.task("default", ["comp", "build", "images", "server"]);
